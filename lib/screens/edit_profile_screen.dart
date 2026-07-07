4df7ca82af81eb67588475dc506e6b9ec97321a5
Aniket Singh <aniket8500singh@gmail.com>
2026-07-07 18:14:28 +0530

    Add Android network permissions

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
 
-class EditProfileScreen extends StatelessWidget {
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../models/user_model.dart';
+import '../services/firestore_service.dart';
+
+class EditProfileScreen extends StatefulWidget {
   const EditProfileScreen({super.key});
+
+  @override
+  State<EditProfileScreen> createState() =>
+      _EditProfileScreenState();
+}
+
+class _EditProfileScreenState
+    extends State<EditProfileScreen> {
+
+  final TextEditingController nameController =
+      TextEditingController();
+
+  final TextEditingController phoneController =
+      TextEditingController();
+
+  final FirestoreService _firestoreService = 
+      FirestoreService();
+
+  @override
+void initState() {
+  super.initState();
+  loadUser();
+}
+
+Future<void> loadUser() async {
+  final uid = FirebaseAuth.instance.currentUser!.uid;
+
+  UserModel? user =
+      await _firestoreService.getUser(uid);
+
+  if (user != null) {
+    nameController.text = user.fullName;
+    phoneController.text = user.phone;
+
+    setState(() {});
+  }
+}
 
   @override
   Widget build(BuildContext context) {
@@ -10,13 +52,41 @@ class EditProfileScreen extends StatelessWidget {
         title: const Text("Edit Profile"),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          "Edit Profile Coming Soon",
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-          ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                labelText: "Full Name",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            TextField(
+              controller: phoneController,
+              keyboardType: TextInputType.phone,
+              decoration: const InputDecoration(
+                labelText: "Phone Number",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text("Save Changes"),
+              ),
+            ),
+          ],
         ),
       ),
     );
